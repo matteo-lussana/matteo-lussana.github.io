9661c9365ed79277536ede79a391911c05cf3f95
matteo-lussana <matteo.lussana@mail.polimi.it>
2024-01-29 01:49:41 +0100

    finish animation, button and table management

--- AlcolTest/main.js
+++ AlcolTest/main.js
@@ -1,13 +1,24 @@
 function addDrink(){
     table = document.querySelector(".input-table");
     line = document.createElement('tr');
+    remove_btn = document.createElement('button');
     drink = document.createElement('input');
     drink.type = "number";
+    drink.className = "input-drink"
     time = document.createElement('input');
     time.type = "time";
+    time.className = "input-time"
+    remove_btn.setAttribute("onClick", "removeDrink()");
     line.appendChild(drink)
     line.appendChild(time)
+    line.appendChild(remove_btn)
     table.appendChild(line)
+}
+
+function removeDrink(){
+    const btn = event.target
+    console.log(btn.parentElement)
+    btn.parentNode.parentNode.removeChild(btn.parentNode)
 }
 
 function compute(){
@@ -25,7 +36,16 @@ function compute(){
     alc_time.forEach(d => {
         sum += d[0];
     });
-    console.log(alc_time)
-    alcolValue = document.querySelector(".alcool");
+    sum = sum.toFixed(2)
+    alcolValue = document.querySelector(".alc-value");
     alcolValue.innerHTML = sum
+    changeAnimatio(sum)
 }
+
+function changeAnimatio(alcolValue){
+    alcolValue = alcolValue*10;
+    alcolValue = Math.round(alcolValue)
+    var animation = document.head.appendChild(document.createElement("style"))
+    animation.innerHTML = '.stickman:before{ content: "";position: absolute; width: 400px; height: 400px; background-color: rgb(107, 14, 30); left:50%; transform: translateX(-50%); border-radius: 40%; animation: a'+alcolValue+' 5s infinite; }'
+}
+

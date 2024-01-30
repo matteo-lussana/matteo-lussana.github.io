789de3c56a83e1b2ee8a15a5a9853104d216d103
matteo-lussana <matteo.lussana@mail.polimi.it>
2024-01-30 15:56:43 +0100

    add setting zone (weigth, sex, food)

--- AlcolTest/main.js
+++ AlcolTest/main.js
@@ -1,3 +1,81 @@
+window.onload = function(){
+    setting = document.querySelector(".input-setting");
+    //sex radio btn
+    label_sex_m = document.createElement("label")
+    label_sex_m.className = "label"
+    label_sex_m.innerHTML = "male:"
+    sex_m = document.createElement("input")
+    sex_m.type = "radio"
+    sex_m.name = "sex"
+
+    label_sex_f = document.createElement("label")
+    label_sex_f.className = "label"
+    label_sex_f.innerHTML = "female:"
+    sex_f = document.createElement("input")
+    sex_f.type = "radio"
+    sex_f.name = "sex"
+
+    setting.appendChild(label_sex_m)
+    setting.appendChild(sex_m)
+    setting.appendChild(label_sex_f)
+    setting.appendChild(sex_f)
+
+    //food radio btn
+    br1 = document.createElement("br")
+    
+    label_w_food = document.createElement("label")
+    label_w_food.className = "label"
+    label_w_food.innerHTML = "with food:"
+    w_food = document.createElement("input")
+    w_food.type = "radio"
+    w_food.name = "food"
+
+    label_wo_food = document.createElement("label")
+    label_wo_food.className = "label"  
+    label_wo_food.innerHTML = "without food:"
+    wo_food = document.createElement("input")
+    wo_food.type = "radio"
+    wo_food.name = "food"
+
+    setting.appendChild(br1)
+    setting.appendChild(label_w_food)
+    setting.appendChild(w_food)
+    setting.appendChild(label_wo_food)
+    setting.appendChild(wo_food)
+
+    //weigth slider
+    br2 = document.createElement("br")
+    label_weigth = document.createElement("label")
+    label_weigth.className = "label"  
+    label_weigth.innerHTML = "weigth:"
+    weigth = document.createElement("input")
+    weigth.type = "range"
+    weigth.min = 20
+    weigth.max = 100
+    //weigth.setAttribute("input", "setSlider()")
+
+    weigth.addEventListener('input', () => {
+
+        label = document.querySelector(".label-slider")
+        label.innerHTML = event.target.value
+    
+    });
+
+    text = document.createElement("label")
+    text.className = "label label-slider"
+
+    setting.appendChild(br2)
+    setting.appendChild(label_weigth)
+    setting.appendChild(weigth)
+    setting.appendChild(text)
+    
+}
+
+function setSlider(){
+    label = document.querySelector(".label-slider")
+    label.innerHTML = event.target.value
+}
+
 function addDrink(){
     table = document.querySelector(".input-table");
     line = document.createElement('tr');
@@ -45,22 +123,34 @@ function removeDrink(){
 function compute(){
     list = Array.from(document.querySelector(".input-table").children);
 
-    alc_time = []
+    
+    Perc = []
+    Vol = []
+    Time = []
 
     list.forEach(d => {
         info = Array.from(d.children);
-        Alc = info[0].valueAsNumber
-        Time = info[1].value
-        alc_time.push([Alc, Time])
+        Perc.push(info[0].valueAsNumber);
+        Vol.push(parseInt(info[1].value));
+
+        time_str = (info[2].value).split(":")[0]
+        Time.push(parseInt(time_str));
     });
-    sum = 0;
-    alc_time.forEach(d => {
-        sum += d[0];
-    });
-    sum = sum.toFixed(2)
+    let date = new Date()
+    Time.push(date.getHours())
+    console.log(Perc)
+    console.log(Vol)
+    console.log(Time)
+
+    alc = 0
+    for(var i=0; i<Time.length-1; i++){
+        alc = 0.0001785*Perc[i]*Vol[i];
+        alc = alc - 0.12*(Time[i+1]-Time[i])
+    }
+    alc = alc.toFixed(2)
     alcolValue = document.querySelector(".alc-value");
-    alcolValue.innerHTML = sum
-    changeAnimatio(sum)
+    alcolValue.innerHTML = alc
+    changeAnimatio(alc)
 }
 
 function changeAnimatio(alcolValue){
@@ -69,4 +159,3 @@ function changeAnimatio(alcolValue){
     var animation = document.head.appendChild(document.createElement("style"))
     animation.innerHTML = '.stickman:before{ content: "";position: absolute; width: 400px; height: 400px; background-color: rgb(107, 14, 30); left:50%; transform: translateX(-50%); border-radius: 40%; animation: a'+alcolValue+' 5s infinite; }'
 }
-
